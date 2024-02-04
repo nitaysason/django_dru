85d7042784f1cf60d1838397bf4f9eed2837f18b
nitaysason <nitaysason@gmail.com>
2024-02-04 13:47:44 +0200

    image

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     
     path('drugs/', views.drugs_view),
     path('drugs/<int:id>', views.drugs_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
