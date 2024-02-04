5f59ae78510a39c98b9b730e3c190498b516f82e
nitaysason <nitaysason@gmail.com>
2024-02-04 13:25:32 +0200

    pilow

--- base/models.py
+++ base/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Drugs(models.Model):
     fName = models.CharField(max_length=50,null=True,blank=True)
     price = models.DecimalField(max_digits=5,decimal_places=2)
+    image = models.ImageField(null=True,blank=True,default='/placeholder.png')
     createdTime=models.DateTimeField(auto_now_add=True)
     fields =['fName','price']
  
